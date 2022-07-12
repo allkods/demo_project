19e5ca895e801a6342418ba1f8fdf0d21108be46
sahil raj <sahilrajkashyap4@gmail.com>
2022-07-12 19:59:58 +0530

    exam system

--- dist/controllers/information.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const view = (req, res, next) => {
-    res.render('information');
-};
-const post = (req, res, next) => {
-    res.end("todo");
-};
-exports.default = { view, post };
